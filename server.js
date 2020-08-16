567569c2a997a48a89db06df0d3445f14cfadfed
Joseph Yeh <josephycyeh@gmail.com>
2020-08-16 01:30:30 -0700

    yes:

--- server.js
+++ server.js
@@ -29,11 +29,12 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static("build"));
- 
+    
+    app.use(express.static("client/build"));
+    const path = require('path')
  
     app.get("*", (req, res) => {
-         res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
      });
  
    }
