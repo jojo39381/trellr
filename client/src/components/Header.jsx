bcf918bf319aa11507505c146d84c19f4a0d1dc9
Joseph Yeh <josephycyeh@gmail.com>
2020-08-16 03:23:47 -0700

    yex

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 function Header() {
     return (
         <header>
-            <h1 style={{textAlign:'center'}}>Joseph's Board</h1>
+            <h1 style={{textAlign:'center'}}>Malia's Board</h1>
         </header>
     )
 }
