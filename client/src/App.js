92acf11069017431977274306da94d184269bc0d
Joseph Yeh <josephycyeh@gmail.com>
2020-08-17 21:45:49 -0700

    a

--- client/src/App.js
+++ client/src/App.js
@@ -135,7 +135,7 @@ function App() {
         console.log("asd")
         axios.get("/weather")
         .then(response => {
-            console.log(response.data)
+           
             // setWeather({
             //     weather: response.data.
             // })
