119b661585b539c8ae666702e66cd3c69c2fef6f
Joseph Yeh <josephycyeh@gmail.com>
2020-08-17 21:51:59 -0700

    a

--- routes/exercises.js
+++ routes/exercises.js
@@ -57,7 +57,7 @@ router.route('/update/:id').post((req, res) => {
 
 
 router.route('/weather').get((req, res) => {
- 
+  console.log("success")
   axios.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyA-bhkcpYuV5V8r-HtcQq6tE0saL-j93ko")
   .then(response => {
       axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + response.data.location.lat + "&lon=" + response.data.location.lng + "&units=imperial&exclude=minutely,hourly,daily&appid=da9df0aa55c4c2692212c2669fa3e530")
@@ -67,7 +67,7 @@ router.route('/weather').get((req, res) => {
             weather:response.data.current.weather[0].main
             
           }
-          console.log(weather)
+          
           res.json(weather)
           
       })
